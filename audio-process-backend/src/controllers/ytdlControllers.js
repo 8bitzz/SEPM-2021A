418e74f750fd172954fa42fa4e3ae00a303f35bf
dinhlongviolin1 <dinhlongviolin1@gmail.com>
2021-04-23 09:59:18 +0700

    add backend for audio process

---
+++ audio-process-backend/src/controllers/ytdlControllers.js
@@ -0,0 +1,14 @@
+// let a = async () => {
+//     let b = await ytdownload.singleDownload(url);
+//     console.log(b);
+// };
+// a();
+
+// ytdownload.getVideosFromPlayList(url, (data) => {
+//     if (data.code === 1) {
+//         return console.log(data.error);
+//     }
+//     ytdownload.downloadVideoFromList(data.output, (output) => {
+//         console.log(output);
+//     });
+// });
