6128cf051e0583648745eb6084c9f39688c26c3e
Nghia Tran <vinhnghiatran@gmail.com>
2021-04-24 10:51:07 +0700

    Transcript long audio to Google Speech To Text

--- audio-process-backend/src/services/google-speech-api/main.js
+++ audio-process-backend/src/services/google-speech-api/main.js
@@ -1,5 +1,41 @@
-const processAudio = () => {
-    console.log("Processing audio!");
+// Imports the Google Cloud client library
+const speech = require('@google-cloud/speech');
+const { getURIGoogleStorage } = require("../../utils/main");
+
+// Creates a client
+const client = new speech.SpeechClient();
+
+const processAudio = (config) => {
+
+    // Get all data
+    const fileName = config.fileName;
+    const gcsUri = getURIGoogleStorage(fileName);
+    const encoding = config.encoding ?? 'MP3';
+    const sampleRateHertz = config.sampleRateHertz ?? 16000;
+    const languageCode = config.languageCode ?? 'en-US';
+
+    // Google Speech Request
+    const request = {
+        config: {
+            encoding: encoding,
+            sampleRateHertz: sampleRateHertz,
+            languageCode: languageCode,
+        },
+        audio: {
+            uri: gcsUri,
+        }
+    };
+
+    // Detects speech in the audio file. This creates a recognition job that you
+    // can wait for now, or get its result later.
+    const [operation] = await client.longRunningRecognize(request);
+    // Get a Promise representation of the final result of the job
+    const [response] = await operation.promise();
+    const transcription = response.results
+        .map(result => result.alternatives[0].transcript)
+        .join('\n');
+    console.log(`Transcription: ${transcription}`);
+    return transcription;
 };
 
 module.exports = {
--- audio-process-backend/src/utils/main.js
+++ audio-process-backend/src/utils/main.js
@@ -2,6 +2,11 @@ const getGoogleCredentialKeyFile = () => {
   return `${process.cwd()}\/${process.env.EDU_SEARCH_CREDENTIAL_KEY}`;
 };
 
+const getURIGoogleStorage = (fileName) => {
+  return `gs://${process.env.GOOGLE_STORAGE_AUDIO_FOLDER}\/${fileName}`;
+};
+
 module.exports = {
   getGoogleCredentialKeyFile,
+  getURIGoogleStorage,
 };
