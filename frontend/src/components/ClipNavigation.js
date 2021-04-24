03a076010bd3cf70cad902ca5f8106935e05eabf
hulemihi <hulemihi@gmail.com>
2021-04-24 12:53:15 +0700

    add clip nav

--- frontend/src/components/ClipNavigation.js
+++ frontend/src/components/ClipNavigation.js
@@ -5,45 +5,6 @@ import NavigateNextOutlinedIcon from '@material-ui/icons/NavigateNextOutlined';
 import FirstPageIcon from '@material-ui/icons/FirstPage';
 import LastPageIcon from '@material-ui/icons/LastPage';
 
-
-// class ClipNavigation extends React.Component {
-//     constructor(props) {
-//       super(props);
-//       this.state = {
-//         page: 0
-//       };
-//     }
-  
-//     render() {
-//       return (
-//         // <div>
-//         //   <p>You clicked {this.state.count} times</p>
-//         //   <button onClick={() => this.setState({ count: this.state.count + 1 })}>
-//         //     Click me
-//         //   </button>
-//         // </div>
-//         <div>
-//             <IconButton><FirstPageIcon/></IconButton>
-//               <IconButton onClick={() => this.setPage(this.state.page - 1)}><NavigateBeforeOutlinedIcon/></IconButton>
-              
-//               <p> {this.state.page} </p>
-//               <IconButton onClick={() => this.setPage(this.state.page + 1)}><NavigateNextOutlinedIcon/></IconButton>
-//               <IconButton><LastPageIcon/></IconButton> 
-//         </div>
-//       );
-//     }
-//   }
-
-// const ClipNavigation = ({page, onClick}) => {
-//     return {
-//         <IconButton><FirstPageIcon/></IconButton>
-//               <IconButton onClick={() => this.setPage(this.state.page - 1)}><NavigateBeforeOutlinedIcon/></IconButton>
-              
-//               <p> {this.state.page} </p>
-//               <IconButton onClick={() => this.setPage(this.state.page + 1)}><NavigateNextOutlinedIcon/></IconButton>
-//               <IconButton><LastPageIcon/></IconButton> 
-//     }
-// }
     class ClipNavigation extends React.Component {
         constructor(props) {
           super(props);
@@ -55,10 +16,10 @@ import LastPageIcon from '@material-ui/icons/LastPage';
         render() {
             return (
                 <div>
-                    <IconButton  onClick={() => this.setCount({ count: this.state.count + 1 })}>
+                    <IconButton  onClick={() => this.setState({ count: this.state.count + 1 })}>
                         <NavigateNextOutlinedIcon/>
                     </IconButton >
-                    <IconButton  onClick={() => this.setCount({ count: this.state.count - 1 })}>
+                    <IconButton  onClick={() => this.setState({ count: this.state.count - 1 })}>
                         <NavigateBeforeOutlinedIcon/>
                     </IconButton >
                 </div>
