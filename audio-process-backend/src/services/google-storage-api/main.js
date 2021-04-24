f2a44f714973adbba7057ad7677e25f993108930
Nghia Tran <vinhnghiatran@gmail.com>
2021-04-24 10:06:49 +0700

    Better file paths

--- audio-process-backend/src/services/google-storage-api/main.js
+++ audio-process-backend/src/services/google-storage-api/main.js
@@ -2,8 +2,12 @@ const { Storage } = require('@google-cloud/storage');
 const storage = new Storage();
 const bucketName = process.env.GOOGLE_STORAGE_BUCKET_NAME;
 
+const getFilePath = (fileName) => {
+  return `${process.cwd()}\/assets\/${fileName}`;
+};
+
 const uploadAudio = async (fileName) => {
-  const filePath = "../../../assets/" + fileName;
+  const filePath = getFilePath(fileName);
   const destFileName = "audio/" + fileName;
   await storage.bucket(bucketName).upload(filePath, {
     destination: destFileName,
