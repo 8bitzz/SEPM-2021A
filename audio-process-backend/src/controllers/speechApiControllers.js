c4e58b281cf5d367b71db5bf5f52fdff573c8b9c
Nghia Tran <vinhnghiatran@gmail.com>
2021-04-24 11:15:26 +0700

    Basic check for Speech API

--- audio-process-backend/src/controllers/speechApiControllers.js
+++ audio-process-backend/src/controllers/speechApiControllers.js
@@ -1,11 +1,30 @@
+const Video = require("../models/video");
+
 const transcriptSingleAudio = async (req, res) => {
   try {
+      // Validate URL
+      const videoID = req.body.videoID;
+      if (!videoID || videoID.length === 0) {
+        return res.status(400).json({ error: "Missing videoID in the Body!" });
+      }
+      
+      // Check if the video exists in the mongodb
+      let videos = await Video.find({ id: videoID }).exec();
+      if (videos.length === 0) {
+        return res.status(400).json({ error: "Could not find Video ID in the database!" });
+      }
+
+      // Check if it is processed
+      const video = videos[0];
+      if (video.processed) {
+        return res.status(400).json({ error: "Video is already processed!" });
+      }
+
       return res.json({ message: "It's transcriptSingleAudio!" });
   } catch (error) {
       return res.status(400).json({ error });
   }
 };
-
 
 module.exports = {
   transcriptSingleAudio,
