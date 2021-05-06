cb84a4a0bb52229eaff37930f39d4b91ca036a8f
dinhlongviolin1 <dinhlongviolin1@gmail.com>
2021-05-06 16:01:13 +0700

    search completed

--- backend/src/controllers/appController.js
+++ backend/src/controllers/appController.js
@@ -1,5 +1,6 @@
-const 
+const Transcript = require("../models/transcript");
 var mockData = require("../mock-data/response.json");
+const Video = require("../models/video");
 
 const searchMock = async (req, res) => {
     try {
@@ -34,28 +35,43 @@ const searchMock = async (req, res) => {
 const search = async (req, res) => {
     try {
         // Validate URL
-        const term = req.query.term;
+        let { term, isExact } = req.query;
         if (!term || term.length === 0) {
             return res.status(400).json({ error: "Missing term in Param Query!" });
         }
-
-        // Response
-        var matchedTranscriptIndxes = [];
-        mockData.transcription.forEach((item, index) => {
-            if (item.transcript.includes(term)) {
-                matchedTranscriptIndxes.push(index);
+        if (!isExact) {
+            return res.status(400).json({ error: "Missing isExact in Param Query!" });
+        }
+        if (isExact.toString() == "true") {
+            term = '"' + term + '"';
+        }
+        let search = await Transcript.find({ $text: { $search: term } }, { score: { $meta: "textScore" } })
+            .sort({ score: { $meta: "textScore" } })
+            .exec();
+        let videoIdSet = new Set();
+        let videoList = [];
+        for (const v of search) {
+            let vidId = v.video.toString();
+            if (!videoIdSet.has(vidId)) {
+                videoIdSet.add(vidId);
+                let foundVid = await Video.findById(vidId)
+                    .select("id duration title transcriptList")
+                    .populate("transcriptList")
+                    .lean()
+                    .exec();
+                foundVid.searchTranscript = [v._id];
+                videoList.push(foundVid);
+            } else {
+                let vidIndex = videoList.findIndex((e) => e._id.toString() == vidId);
+                videoList[vidIndex].searchTranscript = [...videoList[vidIndex].searchTranscript, v._id];
             }
+        }
+        return res.json({
+            term,
+            video_count: videoList.length,
+            search_transcript_count: search.length,
+            video_list_result: videoList,
         });
-
-        const response = {
-            videoURL: "https://www.youtube.com/embed/Oh8aK-0N-9M",
-            numberOfMatchedVideos: 1,
-            numberOfMatchedSentence: matchedTranscriptIndxes.count,
-            originalTranscription: mockData.transcription,
-            matchingTranscriptionIndexs: matchedTranscriptIndxes,
-        };
-
-        return res.json(response);
     } catch (error) {
         return res.status(400).json({ error });
     }
@@ -63,5 +79,5 @@ const search = async (req, res) => {
 
 module.exports = {
     search,
-    searchMock
+    searchMock,
 };
