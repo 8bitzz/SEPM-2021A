97a6870828ea1bd934595d13499a10c0eb51309f
hulemihi <hulemihi@gmail.com>
2021-05-23 17:51:52 +0700

    modify User button

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -7,6 +7,9 @@ import AccountCircle from "@material-ui/icons/AccountCircle";
 import IconButton from "@material-ui/core/IconButton";
 import InputBase from "@material-ui/core/InputBase";
 import SearchIcon from "@material-ui/icons/Search";
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Divider from "@material-ui/core/Divider";
 
 import EduSearchButton from "./EduSearchButton";
 import SignOutButton from "./SignOutButton";
@@ -33,44 +36,45 @@ const useStyles = makeStyles((theme) =>
       // flexGrow: 1,
     },
     search: {
-        position: 'relative',
-        borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-          backgroundColor: fade(theme.palette.common.white, 0.25),
-        },
-        marginLeft: 0,
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-          marginLeft: theme.spacing(1),
-          width: 'auto',
-        },
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
     },
     searchIcon: {
-        padding: theme.spacing(0, 2),
-        height: '100%',
-        position: 'absolute',
-        pointerEvents: 'none',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
     },
     inputRoot: {
-        color: 'inherit',
+      color: 'inherit',
     },
     inputInput: {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('sm')]: {
-          width: '20ch',
-          // '&:focus': {
-          //   width: '20ch',
-          // },
-        },
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '20ch',
+        // '&:focus': {
+        //   width: '20ch',
+        // },
+      },
     },
+   
   })
 );
 
@@ -79,16 +83,25 @@ const LinkWrap = styled(Link)`
   color: #ffffff;
 `;
 
-const NavBar = ({searchTerm, onSearch, onSubmit}) => {
+const NavBar = ({ searchTerm, onSearch, onSubmit }) => {
   return (
     <AuthUserContext.Consumer>
-      {(authUser) => (authUser ? <NavBarAuth searchTerm={searchTerm} onSearch={onSearch} onSubmit={onSubmit}/> : <NavBarNonAuth searchTerm={searchTerm} onSearch={onSearch} onSubmit={onSubmit}/>)}
+      {(authUser) => (authUser ? <NavBarAuth searchTerm={searchTerm} onSearch={onSearch} onSubmit={onSubmit} /> : <NavBarNonAuth searchTerm={searchTerm} onSearch={onSearch} onSubmit={onSubmit} />)}
     </AuthUserContext.Consumer>
   );
 };
 
 const NavBarAuth = (props) => {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <div className={classes.root}>
@@ -116,12 +129,36 @@ const NavBarAuth = (props) => {
                 autoFocus
               />
             </form>
-            
+
           </div>
           <div>
-            <SignOutButton />
-            <IconButton className={classes.user} color="inherit">
+
+            <IconButton
+              aria-haspopup="true"
+              onClick={handleMenu}
+              color="inherit">
               <AccountCircle />
+              <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}>
+                <MenuItem>User Email</MenuItem>
+                <Divider />
+                <MenuItem onclick={event => window.location.href = '/savedclips'}>Saved Clip</MenuItem>
+                <MenuItem onclick={event => window.location.href = '/notes'}>Notes</MenuItem>
+                <Divider />
+                <MenuItem><SignOutButton /></MenuItem>
+
+              </Menu>
             </IconButton>
           </div>
         </Toolbar>
